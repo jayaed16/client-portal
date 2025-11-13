7fa7e02e1c9c7717e8782f5d142dce0248e16c2f
jayaed16 <143442312+jayaed16@users.noreply.github.com>
2025-11-13 07:59:07 -0600

    initial portal setup  with working data base

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_APP.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_APP.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCuqEph59tw9Dhsjk3gqY6q69KY07RYG38",
+  authDomain: "jed-client-portal.firebaseapp.com",
+  projectId: "jed-client-portal",
+  storageBucket: "jed-client-portal.firebasestorage.app",
+  messagingSenderId: "397466183421",
+  appId: "1:397466183421:web:2878ca752e84ec186c0094"
 };
 
 const app = initializeApp(firebaseConfig);
